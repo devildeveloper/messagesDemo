a00444788c9adfcefb72a7b398568dc018c30420
Miguel Augusto Aliaga Pantoja <maliaga.pantoja@gmail.com>
2016-02-02 22:17:39 -0500

    new events

--- index.js
+++ index.js
@@ -9,16 +9,18 @@ io.on('connection',function(socket){
 	console.log('new user connected');
 	socket.on('broadcast',function(data){
 		if(data.group && data.message){
-			socket.broadcast.to(data.group).emit(data.message);
+			socket.broadcast.to(data.group).emit('notification',data.message);
+			socket.emit('success',{message:'mensaje enviado'});
 		}else{
-			socket.emit('error',{message:'group and message are required'});
+			socket.emit('err',{message:'group and message are required'});
 		}
 	});
 	socket.on('join',function(data){
 		if(data.group){
 			socket.join(data.group);
+			socket.emit('success',{message:'joined to'+data.group});
 		}else{
-			socket.emit('error',{message:'group is required'});
+			socket.emit('err',{message:'group is required'});
 		}
 	})
 })
